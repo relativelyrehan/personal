dcf213d43fe040748e8294b180f69475a8d80b38
rehan__alam <rehan.alam@gida.io>
2022-07-03 21:12:03 +0530

    Personal website 👾

---
+++ src/utils/bin/commands.ts
@@ -0,0 +1,141 @@
+// List of commands that do not require API calls
+
+import * as bin from './index';
+
+import config from '../../../config.json';
+
+// Help
+export const help = async (args: string[]): Promise<string> => {
+  const commands = Object.keys(bin).sort().join(', ');
+  var c = '';
+  for (let i = 1; i <= Object.keys(bin).sort().length; i++) {
+    if (i % 7 === 0) {
+      c += Object.keys(bin).sort()[i - 1] + '\n';
+    } else {
+      c += Object.keys(bin).sort()[i - 1] + ' ';
+    }
+  }
+  return `Welcome! Here are all the available commands:
+\n${c}\n
+[tab]: trigger completion.
+[ctrl+l]/clear: clear terminal.\n
+Type 'sumfetch' to display summary.
+`;
+};
+
+// Redirection
+export const repo = async (args: string[]): Promise<string> => {
+  window.open(`${config.repo}`);
+  return 'Opening Github repository...';
+};
+
+// About
+export const about = async (args: string[]): Promise<string> => {
+  return `Hi, I am ${config.name}. 
+Welcome to my website!
+More about me:
+'sumfetch' - short summary.
+'resume' - my latest resume.
+'readme' - my github readme.`;
+};
+
+export const resume = async (args: string[]): Promise<string> => {
+  window.open(`${config.resume_url}`);
+  return 'Opening resume...';
+};
+
+// Contact
+export const email = async (args: string[]): Promise<string> => {
+  window.open(`mailto:${config.email}`);
+  return `Opening mailto:${config.email}...`;
+};
+
+export const github = async (args: string[]): Promise<string> => {
+  window.open(`https://github.com/${config.social.github}/`);
+
+  return 'Opening github...';
+};
+
+export const linkedin = async (args: string[]): Promise<string> => {
+  window.open(`https://www.linkedin.com/in/${config.social.linkedin}/`);
+
+  return 'Opening linkedin...';
+};
+
+// Search
+export const google = async (args: string[]): Promise<string> => {
+  window.open(`https://google.com/search?q=${args.join(' ')}`);
+  return `Searching google for ${args.join(' ')}...`;
+};
+
+export const duckduckgo = async (args: string[]): Promise<string> => {
+  window.open(`https://duckduckgo.com/?q=${args.join(' ')}`);
+  return `Searching duckduckgo for ${args.join(' ')}...`;
+};
+
+export const bing = async (args: string[]): Promise<string> => {
+  window.open(`https://bing.com/search?q=${args.join(' ')}`);
+  return `Wow, really? You are using bing for ${args.join(' ')}?`;
+};
+
+// Typical linux commands
+export const echo = async (args: string[]): Promise<string> => {
+  return args.join(' ');
+};
+
+export const whoami = async (args: string[]): Promise<string> => {
+  return `${config.ps1_username}`;
+};
+
+export const ls = async (args: string[]): Promise<string> => {
+  return `a
+bunch
+of
+fake
+directories`;
+};
+
+export const date = async (args: string[]): Promise<string> => {
+  return new Date().toString();
+};
+
+export const vi = async (args: string[]): Promise<string> => {
+  return `woah, you still use 'vi'? just try 'vim'.`;
+};
+
+export const vim = async (args: string[]): Promise<string> => {
+  return `'vim' is so outdated. how about 'nvim'?`;
+};
+
+export const nvim = async (args: string[]): Promise<string> => {
+  return `'nvim'? too fancy. why not 'emacs'?`;
+};
+
+export const emacs = async (args?: string[]): Promise<string> => {
+  return `you know what? just use vscode.`;
+};
+
+export const vscode = async (args?: string[]): Promise<string> => {
+  return `sublime text is the best!!`;
+};
+
+export const sudo = async (args?: string[]): Promise<string> => {
+  window.open('https://www.youtube.com/watch?v=mAFMJ1LnQu8', '_blank'); // ...I'm sorry
+  return `Permission denied: with little power comes... no responsibility? `;
+};
+
+// Banner
+export const banner = (args?: string[]): string => {
+  return `
+█████        ███                       ███████████                                   
+░░███        ░░░                       ░█░░░███░░░█                                   
+ ░███        ████  █████ █████  ██████ ░   ░███  ░   ██████  ████████  █████████████  
+ ░███       ░░███ ░░███ ░░███  ███░░███    ░███     ███░░███░░███░░███░░███░░███░░███ 
+ ░███        ░███  ░███  ░███ ░███████     ░███    ░███████  ░███ ░░░  ░███ ░███ ░███ 
+ ░███      █ ░███  ░░███ ███  ░███░░░      ░███    ░███░░░   ░███      ░███ ░███ ░███ 
+ ███████████ █████  ░░█████   ░░██████     █████   ░░██████  █████     █████░███ █████
+░░░░░░░░░░░ ░░░░░    ░░░░░     ░░░░░░     ░░░░░     ░░░░░░  ░░░░░     ░░░░░ ░░░ ░░░░░ 
+
+Type 'help' to see the list of available commands.
+`;
+};
---
+++ src/utils/bin/sumfetch.ts
@@ -0,0 +1,24 @@
+import config from '../../../config.json';
+
+const sumfetch = async (args: string[]): Promise<string> => {
+  return `
+           ▄▓▓▓▓▓▓▓▓▓▓▓▓▓▓▄                  sumfetch
+        ▄▓▓▀ ▄▓▓▀▓▓▓▀▓▓▄ ▀▀▓▓▄              -----------
+      ▓▓▀  ▄▓▀   ▐▓▓  ▀▓▓    ▓▓▄             ABOUT
+    ▄▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓            ${config.name}
+   ▓▓     ▓▓▓    ▐▓▓    ▐▓▓     ▓▓           <u><a href="${config.resume_url}" target="_blank">resume</a></u>
+▐▓▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▓       爵 <u><a href="${config.repo}" target="_blank">Github repo</a></u>
+▐▓                                 ▐▓       -----------
+▐▓       > RELATIVELYREHAN         ▐▓        CONTACT 
+▐▓                                 ▐▓        <u><a href="mailto:${config.email}" target="_blank">${config.email}</a></u>
+▐▓▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▓        <u><a href="https://github.com/${config.social.github}" target="_blank">github.com/${config.social.github}</a></u>
+   ▓▓      ▐▓▓    ▓▓    ▐▓▓     ▓▓           <u><a href="https://linkedin.com/in/${config.social.linkedin}" target="_blank">linkedin.com/in/${config.social.linkedin}</a></u>
+    ▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓           -----------
+      ▓▓▓   ▐▓▓   ▓▓   ▓▓▓   ▓▓▀             
+        ▀▓▓▄▄ ▀▓▓▄▓▓▄▓▓▓▄▄▓▓▀               
+            ▀▓▓▓▓▓▓▓▓▓▓▓▀▀                  
+
+`;
+};
+
+export default sumfetch;
